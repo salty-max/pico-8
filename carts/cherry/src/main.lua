6f6b5b254ded82a3e0eedeb53b7f1940830e5a3e
salty-max <max@jellycat.fr>
2023-09-09 23:34:37 +0200

    chb: ship banking and muzzle flash

--- carts/cherry/src/main.lua
+++ carts/cherry/src/main.lua
@@ -9,9 +9,10 @@ function _init()
   ship_flm = {32, 33, 34, 35 ,36}
   flm_idx = 1
 
+  muzzle = 0
+
   bul = {}
   bul_spd = 2
-  bul_ct = 0
 
   debug = {}
 end
@@ -28,7 +29,13 @@ function _update60()
     b.y -= bul_spd
   end
 
+  -- react flame animation
   flm_idx = 1 + flr((frame / 2) % #ship_flm)
+
+  -- muzzle flash
+  if muzzle > 0 then
+    muzzle -= 1
+  end
 end
 
 function _draw()
@@ -38,8 +45,13 @@ function _draw()
     spr(16, b.x, b.y)
   end
 
+  if muzzle > 0 then
+    circfill(ship_x + 3, ship_y - 2, muzzle, 7)
+  end
+  
   spr(ship_spr, ship_x, ship_y)
   spr(ship_flm[flm_idx], ship_x, ship_y + 7)
+
 
   cursor(4, 4)
   color(12)
